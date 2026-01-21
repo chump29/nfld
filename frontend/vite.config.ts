680963ec82d3a09953ad1fba822c396258431389
Chris <chris@postfmly.com>
2026-01-20 23:30:31 -0600

    build: using simpleHtml plugin

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,9 +1,21 @@
 import react from "@vitejs/plugin-react"
-import versionPlugin from "vite-plugin-package-version"
+import version from "vite-plugin-package-version"
+import simpleHtml from "vite-plugin-simple-html"
 import { defineConfig } from "vitest/config"
 
 export default defineConfig({
-  plugins: [react(), versionPlugin()],
+  plugins: [
+    react(),
+    simpleHtml({
+      inject: {
+        data: {
+          title: "NFLd"
+        }
+      },
+      minify: true
+    }),
+    version()
+  ],
   test: {
     environment: "jsdom",
     globals: true,
